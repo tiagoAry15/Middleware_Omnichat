f1b6a3b7c34528115c8b08dab055328a2ac74f54
mateusb12 <mateus_b09@hotmail.com>
2023-05-12 19:18:30 -0300

    eeeeeeeeee

--- pizzaGPT.py
+++ pizzaGPT.py
@@ -1,3 +1,10 @@
+import os
+
+import openai
+from dotenv import load_dotenv
+
+
 class pizzaGPT:
     def __init__(self):
-        pass
+        load_dotenv()
+        openai.api_key = os.environ["OPENAI_API_KEY"]
