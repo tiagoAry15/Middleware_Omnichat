94e9c3b2cf861b753d73c2f21508d155563385a0
mateusb12 <mateus_b09@hotmail.com>
2023-05-12 19:24:09 -0300

    eeeeeeeeee

--- pizzaGPT.py
+++ pizzaGPT.py
@@ -8,3 +8,19 @@ class pizzaGPT:
     def __init__(self):
         load_dotenv()
         openai.api_key = os.environ["OPENAI_API_KEY"]
+
+    def get_response(self, prompt: str):
+        return openai.Completion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": "user", "content": prompt}])
+
+
+def __main():
+    p = pizzaGPT()
+    response = p.get_response("What is the difference between Celsius and Fahrenheit?")
+    print(response)
+    return response
+
+
+if __name__ == '__main__':
+    __main()
