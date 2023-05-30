2555dae9553c2c2b9c205bc2f624ee524e0d13ea
tiago <tiago.ary10@gmail.com>
2023-05-30 16:14:19 -0300

    criação endpoint test loop mensagens + message converter

--- api.py
+++ api.py
@@ -8,6 +8,7 @@ from analyzePizzaIntent import structurePizza, structureDrink, structureFullOrde
 from dialogFlowSession import DialogFlowSession
 from gpt.PizzaGPT import getResponseDefaultGPT
 from intentManipulation.intentManager import IntentManager
+from messageConverter import MessageConverter
 from utils import extractDictFromBytesRequest, sendWebhookCallback, changeDialogflowIntent, _sendTwilioResponse
 from flask_cors import CORS
 
@@ -18,7 +19,8 @@ auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 twilio_phone_number = f'whatsapp:{os.environ["TWILIO_PHONE_NUMBER"]}'
 client = Client(account_sid, auth_token)
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins='http://localhost:5173')
+CORS(app, supports_credentials=True)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 dialogFlowInstance = DialogFlowSession()
 
@@ -47,8 +49,9 @@ def sandbox():  # sourcery skip: use-named-expression
     data = extractDictFromBytesRequest()
     receivedMessage = data.get("Body")[0]
     userNumber = data.get("From")[0]
-    socketio.emit('response', 'teste')
-    print("passou")
+    userMessageJSON = MessageConverter.convert_user_message(data)
+    socketio.emit('user_message', userMessageJSON)
+
     im = IntentManager()
     needsToSignUp = im.needsToSignUp(userNumber)
     if needsToSignUp:
@@ -57,12 +60,16 @@ def sandbox():  # sourcery skip: use-named-expression
         return _sendTwilioResponse(body=botAnswer)
 
     dialogflowResponse = dialogFlowInstance.getDialogFlowResponse(receivedMessage)
+    dialogflowResponseJSON = MessageConverter.convert_dialogflow_message(dialogflowResponse)
+    socketio.emit('dialogflow_message', userMessageJSON)
     secret = dialogFlowInstance.params.get("secret")
     detectedIntent = dialogflowResponse.query_result.intent.display_name
     # IntentManager.process_intent(detectedIntent)
     parameters = dict(dialogflowResponse.query_result.parameters)
     mainResponse = dialogFlowInstance.extractTextFromDialogflowResponse(dialogflowResponse)
     image_url = "https://shorturl.at/lEFT0"
+    socketio.emit('Bot_response', dialogflowResponseJSON)
+
     return _sendTwilioResponse(body=mainResponse, media=None)
 
 
@@ -113,6 +120,139 @@ def send():
     return sendWebhookCallback(botMessage="a")
 
 
+@app.route("/ChaTtest", methods=['GET'])
+def chatTest():
+    dialogflow_message = {
+        "response_id": "db1b18cf-4ed9-4339-bcfd-725cf722b4b7-4c6e80df",
+        "query_result": {
+            "query_text": "Oi",
+            "language_code": "pt-br",
+            "action": "input.welcome",
+            "parameters": {
+                "fields": {
+                    "key": "time-period",
+                    "value": {
+                        "string_value": ""
+                    }
+                },
+                "fields": {
+                    "key": "location",
+                    "value": {
+                        "struct_value": {
+                            "fields": {
+                                "key": "zip-code",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "subadmin-area",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "street-address",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "shortcut",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "island",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "country",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "city",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            },
+                            "fields": {
+                                "key": "business-name",
+                                "value": {
+                                    "string_value": "Oi"
+                                }
+                            },
+                            "fields": {
+                                "key": "admin-area",
+                                "value": {
+                                    "string_value": ""
+                                }
+                            }
+                        }
+                    }
+                },
+                "fields": {
+                    "key": "date-time",
+                    "value": {
+                        "string_value": ""
+                    }
+                }
+            },
+            "all_required_params_present": "True",
+            "fulfillment_text": "Por favor, ligue a API",
+            "fulfillment_messages": {
+                "text": {
+                    "text": "Por favor, ligue a API"
+                }
+            },
+            "intent": {
+                "name": "projects/pizzadobill-rpin/agent/intents/acd8e087-5400-4cf9-95f3-4c681b16b516",
+                "display_name": "Welcome"
+            },
+            "intent_detection_confidence": 1,
+            "diagnostic_info": {
+                "fields": {
+                    "key": "webhook_latency_ms",
+                    "value": {
+                        "number_value": 2889
+                    }
+                }
+            }
+        },
+        "webhook_status": {
+            "code": 14,
+            "message": "Webhook call failed. Error: UNAVAILABLE, State: URL_UNREACHABLE, Reason: UNREACHABLE_5xx, HTTP status code: 502."
+        }
+    }
+    user_message = {
+        "SmsMessageSid": ["SMd9c46a08ff3349af9a93dc2d40d738ff"],
+        "NumMedia": ["0"],
+        "ProfileName": ["Tiago Ary"],
+        "SmsSid": ["SMd9c46a08ff3349af9a93dc2d40d738ff"],
+        "WaId": ["558599663533"],
+        "SmsStatus": ["received"],
+        "Body": ["Oi"],
+        "To": ["whatsapp:+14155238886"],
+        "NumSegments": ["1"],
+        "ReferralNumMedia": ["0"],
+        "MessageSid": ["SMd9c46a08ff3349af9a93dc2d40d738ff"],
+        "AccountSid": ["AC034f7d97b8d5bc62dfa91b519ac43b0f"],
+        "From": ["whatsapp:+558599663533"],
+        "ApiVersion": ["2010-04-01"]
+    }
+    userMessageJSON = MessageConverter.convert_user_message(user_message)
+
+    dialogFlowJSON = MessageConverter.convert_dialogflow_message(dialogflow_message, userMessageJSON['telephone'])
+    for i in range(0, 4):
+        socketio.emit('user_message', userMessageJSON)
+        socketio.emit('dialogflow_message', dialogFlowJSON)
+
+
 @app.route("/staticReply", methods=['POST'])
 def staticReply():
     return sendWebhookCallback("This is a message from the server!")
@@ -161,7 +301,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    socketio.run(app, port=8000, debug=True)
-    print('deu certo')
-    while True:
-        socketio.emit('response', 'teste', broadcast=True)
+    socketio.run(app, port=8000)
