527e08671bc6a80077dc47bcc8702b4eb21d3dc1
mateusb12 <mateus_b09@hotmail.com>
2023-06-08 12:54:10 -0300

    firebase sdk not needed anymore

--- firebaseFolder/firebaseConnection.py
+++ firebaseFolder/firebaseConnection.py
@@ -7,12 +7,31 @@ from firebase_admin import credentials, db
 from dialogFlowSession import singleton
 from references.pathReference import getFirebaseSDKPath
 
+def getFirebaseCredentials():
+    firebase_credentials = {
+        "type": os.environ["FIREBASE_SDK_TYPE"],
+        "project_id": os.environ["FIREBASE_SDK_PROJECT_ID"],
+        "private_key_id": os.environ["FIREBASE_SDK_PRIVATE_KEY_ID"],
+        "private_key": os.environ["FIREBASE_SDK_PRIVATE_KEY"],
+        "client_email": os.environ["FIREBASE_SDK_CLIENT_EMAIL"],
+        "client_id": os.environ["FIREBASE_SDK_CLIENT_ID"],
+        "auth_uri": os.environ["FIREBASE_SDK_AUTH_URI"],
+        "token_uri": os.environ["FIREBASE_SDK_TOKEN_URI"],
+        "auth_provider_x509_cert_url": os.environ["FIREBASE_SDK_AUTH_PROVIDER_X509_CERT_URL"],
+        "client_x509_cert_url": os.environ["FIREBASE_SDK_CLIENT_X509_CERT_URL"]
+    }
+    key = firebase_credentials["private_key"]
+    key = key.replace("-----BEGIN PRIVATE KEY-----", "").replace("-----END PRIVATE KEY-----", "").strip()
+    key = f"-----BEGIN PRIVATE KEY-----\n{key}\n-----END PRIVATE KEY-----"
+    firebase_credentials["private_key"] = key
+    return credentials.Certificate(firebase_credentials)
+
 
 @singleton
 class FirebaseConnection:
     def __init__(self):
         load_dotenv()
-        cred = credentials.Certificate(getFirebaseSDKPath())
+        cred = getFirebaseCredentials()
         self.app = firebase_admin.initialize_app(cred, {"databaseURL": os.getenv("FIREBASE_DATABASE_URL")})
         self.connection = db.reference('/', app=self.app)
 
@@ -60,7 +79,7 @@ class FirebaseConnection:
 
 def __main():
     fc = FirebaseConnection()
-    data = fc.deleteData("users", {"email": "user@example.com", "password": "password"})
+    data = fc.readData("users")
     return
 
 
