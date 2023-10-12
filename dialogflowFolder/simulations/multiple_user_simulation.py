033c54b41e856eb36669127dd03f5cfeea08c62d
mateusb12 <mateus_b09@hotmail.com>
2023-10-11 21:56:21 -0300

    Refactor user_simulation to accept messages
    Refactored `user_simulation` method in `multiple_user_simulation.py` to accept an additional parameter `message_pool`. Instead of having static messages within the method, messages for different users can now be passed in as arguments. This allows for customizable interactions for each user, making testing of the dialogflow more flexible. The respective user messages were added in the main function. Assertion line checking if both users receive the same responses was also removed, to allow the acceptance of different messages per user.

--- dialogflowFolder/simulations/multiple_user_simulation.py
+++ dialogflowFolder/simulations/multiple_user_simulation.py
@@ -4,13 +4,12 @@ import uuid
 from dialogflowFolder.session_manager import SessionManager
 
 
-def user_simulation(user_id: str):
+def user_simulation(user_id: str, message_pool: list):
     manager = SessionManager()
     ds = manager.get_session(user_id)
     ds.initialize_session(user_id)
 
     response_dict = {}
-    message_pool = ["Oi", "Vou querer duas pizzas de frango", "Sim", "Vou querer um guaraná", "Pix"]
     for message in message_pool:
         response = ds.getDialogFlowResponse(message=message)
         bot_answer = response.query_result.fulfillment_text
@@ -25,13 +24,17 @@ def main():
     user1_results = {}
     user2_results = {}
 
+    user1_messages = ["Oi", "Vou querer duas pizzas de frango", "Sim", "Vou querer um guaraná", "Pix"]
+    user2_messages = ["Oi", "Vou querer duas pizzas de calabresa e uma pizza meia pepperoni meia frango", "Sim"
+                      "Vou querer dois guaranás e um suco de laranja", "Dinheiro"]
+
     def user1_thread():
         nonlocal user1_results
-        user1_results = user_simulation(user1_id)
+        user1_results = user_simulation(user1_id, user1_messages)
 
     def user2_thread():
         nonlocal user2_results
-        user2_results = user_simulation(user2_id)
+        user2_results = user_simulation(user2_id, user2_messages)
 
     thread1 = threading.Thread(target=user1_thread)
     thread2 = threading.Thread(target=user2_thread)
@@ -44,9 +47,6 @@ def main():
 
     print("User 1 Results:", user1_results)
     print("User 2 Results:", user2_results)
-
-    assert user1_results == user2_results, "Results are different!"
-    print("Both users received the same responses!")
 
 
 if __name__ == "__main__":
