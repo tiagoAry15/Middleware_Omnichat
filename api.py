55dcdd0ee52bd33567432f8732cd99418a8c48ec
mateusb12 <mateus_b09@hotmail.com>
2023-10-21 10:42:33 -0300

    ops

--- api.py
+++ api.py
@@ -36,7 +36,7 @@ def sandbox():
         botResponse = _get_bot_response_from_user_session(user_message=userMessage, ip_address=ip_address)
         appendMultipleMessagesToFirebase(userMessage=userMessage, botAnswer=botResponse, metaData=metaData)
         socketio.start_background_task(target=emitMessage, message=botResponse)
-        return jsonify({"response": botResponse}), 200
+        return botResponse
     except Exception as e:
         print(e)
         logging.error(e)
