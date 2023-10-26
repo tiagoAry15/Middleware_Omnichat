fd021031757159fe0994b10f87d0f25708abe59b
Mateus <mateus_b09@hotmail.com>
2023-10-26 18:25:00 -0300

    aaaaaaa

--- firebaseFolder/firebase_speisekarte.py
+++ firebaseFolder/firebase_speisekarte.py
@@ -27,7 +27,7 @@ class FirebaseSpeisekarte(FirebaseWrapper):
     def __init__(self, inputFirebaseConnection: FirebaseConnection):
         super().__init__()
         self.firebaseConnection = inputFirebaseConnection
-        self.cache = {}
+        self.data = {}
         self.load_cache()
 
     def updateConnection(self):
@@ -36,6 +36,7 @@ class FirebaseSpeisekarte(FirebaseWrapper):
     def refreshSpeisekarteCache(self) -> bool:
         all_data = self.firebaseConnection.readData()
         save_cache_json(filename="speisekarte_cache.json", data=all_data)
+        self.data = all_data
         return True
 
     def load_cache(self):
@@ -43,19 +44,54 @@ class FirebaseSpeisekarte(FirebaseWrapper):
         if timedelta.days >= 1:
             print("Cache is outdated! Refreshing...")
             self.refreshSpeisekarteCache()
-        return
+        self.data = data
 
-    def createSpeisekarte(self, speisekarte_data: dict):
-        return self.firebaseConnection.writeData(data=speisekarte_data)
+    def createSpeisekarte(self, speisekarte_data: dict) -> bool:
+        existing = self.existing_speisekarte(speisekarte_data)
+        if not existing:
+            self.firebaseConnection.writeData(data=speisekarte_data)
+            return True
+        return False
 
     def createDummySpeisekarte(self):
         dummy_speisekarte = get_current_speisekarte()
         return self.createSpeisekarte(dummy_speisekarte)
 
+    def existing_speisekarte(self, input_speisekarte: dict) -> bool:
+        speisekarte_pool = list(self.data.values())
+        return input_speisekarte in speisekarte_pool
+
+    def read_speisekarte(self, author: str) -> dict or None:
+        speisekarte_pool = list(self.data.values())
+        for item in speisekarte_pool:
+            item_author = item["Autor"].lower()
+            if item_author == author.lower():
+                return item
+        return None
+
+    def update_speisekarte(self, author: str, **kwargs) -> bool or None:
+        speisekarte = self.read_speisekarte(author=author)
+        if not speisekarte:
+            return None
+        for key, value in kwargs.items():
+            speisekarte[key] = value
+        self.firebaseConnection.overWriteData(data=speisekarte)
+        return True
+
+    def delete_speisekarte(self, author: str):
+        # TODO
+        speisekarte = self.read_speisekarte(author=author)
+        if not speisekarte:
+            return None
+        self.firebaseConnection.deleteData(path="", data=speisekarte)
+        return True
+
 
 def __main():
     fc = FirebaseConnection()
     fs = FirebaseSpeisekarte(fc)
+    speisekarte = get_current_speisekarte()
+    fs.delete_speisekarte("bill")
     return
 
 
