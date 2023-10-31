95dfdd2caaffef00d72131ad1b847e1274614cc4
Mateus <mateus_b09@hotmail.com>
2023-10-31 14:23:38 -0300

    Updated conversation cache with random data inconsistencies simulation
    In this commit "conversation_cache.py" file has been updated to simulate random data inconsistencies while pulling data from Firebase. Added functions like "get_inconsistent_conversations", "get_firebase_inconsistent_data" and "convert_to_datetime", which introduce random alterations in the data for simulation purpose.
    
    Message content in 'messagePot' has been changed to give it a more informal tone, in order to simulate a realistic conversation scenario. Some unused import statements have also been removed for cleaner code. Added a function "__get_conversation_unique_id_by_whatsapp_number" that replaces the old "get_conversation_unique_id_by_whatsapp_number" to get conversation unique id.
    
    The "generate_firebase_push_id" is now utilized to generate unique ids for new conversations to ensure the uniqueness of conversation data. Added a function __create_first_conversation to add the first conversation while initializing. Moreover, cleanup and some renaming were done for neatness and consistency across the code.
    
    To manage the data for a specific conversation, class ConversationCache has been provided. Inside it, a function 'delete_conversation' has been added to provide functionality to delete a specific conversation using the whatsapp number.
    
    Finally, the "__main__" function has been updated to reflect these changes.
    
    These modifications are part of an ongoing project to enhance the performance of the chat platform and eliminate possible data inconsistencies.

--- firebaseCache/conversation_cache.py
+++ firebaseCache/conversation_cache.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 from typing import List, Union
 
 from firebaseFolder.firebase_connection import FirebaseConnection
@@ -12,42 +13,92 @@ def get_dummy_conversations():
                                  'messagePot': [
                                      {'body': 'Oii', 'id': '51b7e38f-75bb-452d-8463-cb01d4f34916', 'sender': 'Mateus',
                                       'time': '25/10/2023 10:31'}, {
-                                         'body': 'Não foi possível se conectar ao fulfillment do dialogflow! Por '
-                                                 'favor, ligue a API',
+                                         'body': 'Olá meu querido, tudo bem?',
                                          'from': 'whatsapp', 'phoneNumber': '558599171902', 'sender': 'Bot',
                                          'time': '10:31'}], 'name': 'Mateus', 'phoneNumber': '558599171902',
                                  'status': 'active', 'unreadMessages': 0},
         '-Nhg7rURWmVxTJmPe8GL': {'from': 'whatsapp', 'isBotActive': True, 'lastMessage_timestamp': '2023-10-26 13:20',
                                  'messagePot': [
-                                     {'body': 'oi', 'id': '68cf1641-ede7-4998-a013-12af69099dc4', 'sender': 'Tiago',
+                                     {'body': 'Oii', 'id': '68cf1641-ede7-4998-a013-12af69099dc4', 'sender': 'Tiago',
+                                      'time': '2023-10-26 13:19'},
+                                     {'body': 'Olá! Bem-vindo à Pizza do Bill! Funcionamos das 17h às 22h.\n '
+                                              'Cardápio de pizzas:\n- Calabresa - R$17.50\n- Frango - R$18.90\n- '
+                                              'Portuguesa - R$13.99\n- Margherita - R$15.50\n- Quatro Queijos - '
+                                              'R$16.90\n- Pepperoni - R$19.99\n. \nQual pizza você vai querer?',
+                                      'id': 'ae15a053-a99f-47fd-a034-afdd29f5a8a4', 'sender': 'Bot',
+                                      'time': '2023-10-26 13:19'},
+
+                                     {'body': 'Vou querer uma pizza meio frango meio calabresa',
+                                      'id': 'cfbd675f-4768-4353-8977-6eab09e08a6b', 'sender': 'Tiago',
                                       'time': '2023-10-26 13:19'}, {
-                                         'body': 'Olá! Bem-vindo à Pizza do Bill! Funcionamos das 17h às 22h.\n '
-                                                 'Cardápio de pizzas:\n- Calabresa - R$17.50\n- Frango - R$18.90\n- '
-                                                 'Portuguesa - R$13.99\n- Margherita - R$15.50\n- Quatro Queijos - '
-                                                 'R$16.90\n- Pepperoni - R$19.99\n. \nQual pizza você vai querer?',
-                                         'id': 'ae15a053-a99f-47fd-a034-afdd29f5a8a4', 'sender': 'Bot',
-                                         'time': '2023-10-26 13:19'},
-                                     {'body': 'oi', 'id': 'cfbd675f-4768-4353-8977-6eab09e08a6b', 'sender': 'Tiago',
-                                      'time': '2023-10-26 13:19'}, {
-                                         'body': 'Olá! Bem-vindo à Pizza do Bill! Funcionamos das 17h às 22h.\n '
-                                                 'Cardápio de pizzas:\n- Calabresa - R$17.50\n- Frango - R$18.90\n- '
-                                                 'Portuguesa - R$13.99\n- Margherita - R$15.50\n- Quatro Queijos - '
-                                                 'R$16.90\n- Pepperoni - R$19.99\n. \nQual pizza você vai querer?',
+                                         'body': 'Certo, uma pizza meio frango meio calabresa saindo então!'
+                                                 ' Vai querer bebida?',
                                          'id': 'd656dd23-3c8c-40e8-988a-941ef12d1641', 'sender': 'Bot',
                                          'time': '2023-10-26 13:19'},
-                                     {'body': 'oi', 'id': '35dbbec8-d1b1-4853-aa9d-b146f6cdae96', 'sender': 'Tiago',
+
+                                     {'body': 'Vou querer um suco de laranja',
+                                      'id': '35dbbec8-d1b1-4853-aa9d-b146f6cdae96', 'sender': 'Tiago',
                                       'time': '2023-10-26 13:20'}, {
-                                         'body': 'Olá! Bem-vindo à Pizza do Bill! Funcionamos das 17h às 22h.\n '
-                                                 'Cardápio de pizzas:\n- Calabresa - R$17.50\n- Frango - R$18.90\n- '
-                                                 'Portuguesa - R$13.99\n- Margherita - R$15.50\n- Quatro Queijos - '
-                                                 'R$16.90\n- Pepperoni - R$19.99\n. \nQual pizza você vai querer?',
-                                         'id': '01741c4e-74b5-4f51-9485-34719b1f3304', 'sender': 'Bot',
+                                         'body': 'Maravilha então!', 'sender': 'Bot',
                                          'time': '2023-10-26 13:20'}], 'name': 'Tiago', 'phoneNumber': '558599663533',
                                  'status': 'active', 'unreadMessages': 0}}
 
 
+def get_firebase_inconsistent_data() -> dict:
+    """
+    Simulate pulling modified data from Firebase by introducing random modifications.
+    Represents changes made by other backend replicas.
+    """
+    data = get_dummy_conversations()
+    for unique_id, conversation in data.items():
+        # Randomly modify the last message for simulation
+        if random.choice([True, False]):
+            last_message = conversation["messagePot"][-1]
+            new_message_body = last_message["body"] + " (Modified by Replica)"
+            last_message["body"] = new_message_body
+            conversation["messagePot"][-1] = last_message
+
+            # Update the timestamp of last message
+            timestamp = conversation["lastMessage_timestamp"]
+            timestamp_datetime = convert_to_datetime(timestamp)
+            new_timestamp = timestamp_datetime + datetime.timedelta(minutes=random.randint(1, 60))
+            conversation["lastMessage_timestamp"] = new_timestamp.strftime("%d/%m/%Y %H:%M")
+
+    return data
+
+
 def get_current_timestamp() -> str:
     return datetime.datetime.now().strftime("%d-%b-%Y at %H:%M:%S")
+
+
+def convert_to_datetime(timestamp: str) -> datetime.datetime:
+    """
+    Convert the string timestamp to a datetime object for comparison.
+    """
+    try:
+        # If timestamp is in "dd/mm/yyyy HH:MM" format
+        return datetime.datetime.strptime(timestamp, "%d/%m/%Y %H:%M")
+    except ValueError:
+        try:
+            # If timestamp is in "dd-bbb-yyyy at HH:MM:SS" format
+            return datetime.datetime.strptime(timestamp, "%d-%b-%Y at %H:%M:%S")
+        except Exception as e:
+            raise ValueError("Timestamp format not recognized")
+
+
+def get_inconsistent_conversations() -> dict:
+    """
+    Simulate pulling inconsistent data from Firebase by introducing random inconsistencies.
+    """
+    data = get_dummy_conversations()
+    for unique_id, conversation in data.items():
+        # Randomly change the timestamp for simulation
+        if random.choice([True, False]):
+            timestamp = conversation["lastMessage_timestamp"]
+            timestamp_datetime = convert_to_datetime(timestamp)
+            new_timestamp = timestamp_datetime + datetime.timedelta(minutes=random.randint(1, 60))
+            conversation["lastMessage_timestamp"] = new_timestamp.strftime("%d/%m/%Y %H:%M")
+    return data
 
 
 class ConversationCache:
@@ -60,7 +111,7 @@ class ConversationCache:
     def _cache_miss_refresh_data(self):
         self.data = get_dummy_conversations()
 
-    def get_conversation_unique_id_by_whatsapp_number(self, whatsapp_number: str) -> str or None:
+    def __get_conversation_unique_id_by_whatsapp_number(self, whatsapp_number: str) -> str or None:
         for unique_id in self.data.keys():
             if self.data[unique_id]["phoneNumber"] == whatsapp_number:
                 return unique_id
@@ -68,7 +119,7 @@ class ConversationCache:
 
     def __update_messages_in_conversation(self, messages: Union[dict, List[dict]], whatsappNumber: str):
         """Helper function to update the messagePot for a given WhatsApp number."""
-        unique_id = self.get_conversation_unique_id_by_whatsapp_number(whatsappNumber)
+        unique_id = self.__get_conversation_unique_id_by_whatsapp_number(whatsappNumber)
         conversation = self.data[unique_id]
 
         if isinstance(messages, dict):
@@ -78,38 +129,45 @@ class ConversationCache:
 
         self.data[unique_id] = conversation
 
+    def __create_first_conversation(self):
+        conversation_dict = {"from": "messenger", "isBotActive": True, "lastMessage_timestamp": get_current_timestamp(),
+                             "messagePot": [], "name": "Bill", "phoneNumber": "558599171902", "status": "active",
+                             "unreadMessages": 0}
+        self.firebase_connection.createConversation(conversation_dict)
+
     def append_message_to_whatsapp_number(self, messageData: dict, whatsappNumber: str):
         return self.__update_messages_in_conversation(messageData, whatsappNumber)
 
     def append_multiple_messages_to_whatsapp_number(self, messageData: List[dict], whatsappNumber: str):
         return self.__update_messages_in_conversation(messageData, whatsappNumber)
 
-    def create_first_conversation(self):
-        conversation_dict = {"from": "messenger", "isBotActive": True, "lastMessage_timestamp": get_current_timestamp(),
-                             "messagePot": [], "name": "Bill", "phoneNumber": "558599171902", "status": "active",
-                             "unreadMessages": 0}
-        self.firebase_connection.createConversation(conversation_dict)
-
     def create_conversation(self, whatsapp_number: str, conversation_dict: dict) -> bool:
-        existing_unique_id = self.get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
+        existing_unique_id = self.__get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
         if existing_unique_id:
             return False
         unique_id = generate_firebase_push_id()
         self.data[unique_id] = conversation_dict
 
     def get_conversation(self, whatsapp_number: str) -> dict or None:
-        unique_id = self.get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
+        unique_id = self.__get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
         if not unique_id:
             return None
         return self.data[unique_id]
 
     def update_conversation(self, whatsapp_number: str, conversation_dict: dict) -> bool:
-        unique_id = self.get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
+        unique_id = self.__get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
         if not unique_id:
             return False
         current_conversation = self.data[unique_id]
         for key, value in conversation_dict.items():
             current_conversation[key] = value
+        return True
+
+    def delete_conversation(self, whatsapp_number: str) -> bool:
+        unique_id = self.__get_conversation_unique_id_by_whatsapp_number(whatsapp_number)
+        if not unique_id:
+            return False
+        self.data.pop(unique_id)
         return True
 
 
@@ -119,9 +177,10 @@ def __main():
     cc = ConversationCache(fcm)
     whatsapp_number = "558599171902"
     # conv = cc.get_conversations(whatsapp_number)
-    msgDict = {'body': 'Oii', 'from': 'whatsapp', 'phoneNumber': "558599171902", 'sender': 'Mateus',
-               'time': datetime.datetime.now().strftime('%H:%M')}
-    cc.append_message_to_whatsapp_number(msgDict, whatsapp_number)
+    # msgDict = {'body': 'Oii', 'from': 'whatsapp', 'phoneNumber': "558599171902", 'sender': 'Mateus',
+    #            'time': datetime.datetime.now().strftime('%H:%M')}
+    # cc.append_message_to_whatsapp_number(msgDict, whatsapp_number)
+    inconsistent_data = get_inconsistent_conversations()
     return
 
 
