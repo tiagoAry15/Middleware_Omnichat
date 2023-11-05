194e7fb62a9ffff6c5dbed74fb9edf610ee35c63
Tiago <tiago.ary10@gmail.com>
2023-11-05 16:40:29 -0300

    ta quase

--- app.py
+++ app.py
@@ -155,4 +155,4 @@ for resource in app.router._resources:
              {'*': aiohttp_cors.ResourceOptions(allow_credentials=True, expose_headers="*", allow_headers="*")})
 
 if __name__ == '__main__':
-    web.run_app(app, host='0.0.0.0', port=3000, shutdown_timeout=180)
+    web.run_app(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), shutdown_timeout=180)
